ea6c932c009f86e28a29e6f2a1748de49703b39a
Govind Turkar <govindturkar69@gmail.com>
2026-09-10 15:36:25 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def divide(a, b):
+    return a / b
+
+
+def calculate_average(numbers):
+    total = sum(numbers)
+    return total / len(numbers)
+
+
+if __name__ == "__main__":
+    print(divide(10, 2))
